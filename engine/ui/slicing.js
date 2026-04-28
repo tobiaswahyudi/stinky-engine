43f814a6ad32e34275e96488d01263263fd98d92
Tobias Wahyudi <tobias@medmehealth.com>
2026-04-28 01:10:58 -0400

    three slicing

--- engine/ui/slicing.js
+++ engine/ui/slicing.js
@@ -14,8 +14,6 @@ class NineSlice {
   constructor(canvasManager, sheet, rowStart, colStart, scale) {
     this.canvasManager = canvasManager;
     this.sheet = sheet;
-    this.rowStart = rowStart;
-    this.colStart = colStart;
     this.scale = scale;
 
     // Get segments
@@ -48,14 +46,129 @@ class NineSlice {
     const hCenterSize = width - 2 * cellSize;
     const vCenterSize = height - 2 * cellSize;
 
-    this.canvasManager.drawImage(this.UL, topLeftVec.x, topLeftVec.y, cellSize, cellSize);
-    this.canvasManager.drawImage(this.U, topLeftVec.x + cellSize, topLeftVec.y, hCenterSize, cellSize);
-    this.canvasManager.drawImage(this.UR, topLeftVec.x + cellSize + hCenterSize, topLeftVec.y, cellSize, cellSize);
-    this.canvasManager.drawImage(this.L, topLeftVec.x, topLeftVec.y + cellSize, cellSize, vCenterSize);
-    this.canvasManager.drawImage(this.C, topLeftVec.x + cellSize, topLeftVec.y + cellSize, hCenterSize, vCenterSize);
-    this.canvasManager.drawImage(this.R, topLeftVec.x + cellSize + hCenterSize, topLeftVec.y + cellSize, cellSize, vCenterSize);
-    this.canvasManager.drawImage(this.DL, topLeftVec.x, topLeftVec.y + cellSize + vCenterSize, cellSize, cellSize);
-    this.canvasManager.drawImage(this.D, topLeftVec.x + cellSize, topLeftVec.y + cellSize + vCenterSize, hCenterSize, cellSize);
-    this.canvasManager.drawImage(this.DR, topLeftVec.x + cellSize + hCenterSize, topLeftVec.y + cellSize + vCenterSize, cellSize, cellSize);
+    this.canvasManager.drawImage(
+      this.UL,
+      topLeftVec.x,
+      topLeftVec.y,
+      cellSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.U,
+      topLeftVec.x + cellSize,
+      topLeftVec.y,
+      hCenterSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.UR,
+      topLeftVec.x + cellSize + hCenterSize,
+      topLeftVec.y,
+      cellSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.L,
+      topLeftVec.x,
+      topLeftVec.y + cellSize,
+      cellSize,
+      vCenterSize,
+    );
+    this.canvasManager.drawImage(
+      this.C,
+      topLeftVec.x + cellSize,
+      topLeftVec.y + cellSize,
+      hCenterSize,
+      vCenterSize,
+    );
+    this.canvasManager.drawImage(
+      this.R,
+      topLeftVec.x + cellSize + hCenterSize,
+      topLeftVec.y + cellSize,
+      cellSize,
+      vCenterSize,
+    );
+    this.canvasManager.drawImage(
+      this.DL,
+      topLeftVec.x,
+      topLeftVec.y + cellSize + vCenterSize,
+      cellSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.D,
+      topLeftVec.x + cellSize,
+      topLeftVec.y + cellSize + vCenterSize,
+      hCenterSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.DR,
+      topLeftVec.x + cellSize + hCenterSize,
+      topLeftVec.y + cellSize + vCenterSize,
+      cellSize,
+      cellSize,
+    );
+  }
+}
+
+class ThreeSlice {
+  constructor(canvasManager, sheet, rowStart, colStart, scale) {
+    this.canvasManager = canvasManager;
+    this.sheet = sheet;
+    this.scale = scale;
+
+    // Get segments
+    this.L = sheet[rowStart][colStart];
+    this.C = sheet[rowStart][colStart + 1];
+    this.R = sheet[rowStart][colStart + 2];
+  }
+
+  // If width is less than 2 * cellSize, this will just be wrong. And it will be your fault
+  draw(x, y, width, align = ALIGN.Center, offset = 0) {
+    const cellSize = this.L.c * this.scale;
+    const leftAlignment = align % 3;
+    const topAlignment = Math.floor(align / 3);
+
+    console.log(
+      align,
+      leftAlignment,
+      topAlignment,
+      -leftAlignment / 2,
+      -topAlignment / 2,
+    );
+
+    const offsetVec = new Vec2(1 - leftAlignment, 1 - topAlignment)
+      .normalize()
+      .scale(offset);
+
+    const topLeftVec = new Vec2(-leftAlignment / 2, -topAlignment / 2)
+      .scale(width, cellSize)
+      .add(new Vec2(x, y))
+      .add(offsetVec);
+
+    const hCenterSize = width - 2 * cellSize;
+
+    this.canvasManager.drawImage(
+      this.L,
+      topLeftVec.x,
+      topLeftVec.y,
+      cellSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.C,
+      topLeftVec.x + cellSize,
+      topLeftVec.y,
+      hCenterSize,
+      cellSize,
+    );
+    this.canvasManager.drawImage(
+      this.R,
+      topLeftVec.x + cellSize + hCenterSize,
+      topLeftVec.y,
+      cellSize,
+      cellSize,
+    );
   }
 }
--- game/game.js
+++ game/game.js
@@ -4,6 +4,7 @@ function startGame(game) {
   let renderCount = 0;
 
   const nineslice = new NineSlice(canvas, ASSETS.sprites.DESERT.UI, 0, 9, 4);
+  const threeslice = new ThreeSlice(canvas, ASSETS.sprites.DESERT.UI, 3, 7, 4);
 
   const renderLoop = () => {
     canvas.clear("#4483e2");
@@ -24,6 +25,25 @@ function startGame(game) {
       150 + Math.sin(renderCount * 0.05) * 20,
       160 + Math.sin(renderCount * 0.07) * 30,
       ALIGN.Center,
+    );
+
+    threeslice.draw(
+      100,
+      canvas.height / 2 - 100,
+      150 + Math.sin(renderCount * 0.05) * 20,
+      ALIGN.Left,
+    );
+    threeslice.draw(
+      200,
+      canvas.height / 2,
+      150 + Math.sin(renderCount * 0.05) * 20,
+      ALIGN.Center,
+    );
+    threeslice.draw(
+      300,
+      canvas.height / 2 + 100,
+      150 + Math.sin(renderCount * 0.05) * 20,
+      ALIGN.Right,
     );
 
     setTimeout(renderLoop, 1000 / 60);
